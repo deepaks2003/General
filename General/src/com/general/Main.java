6a6a62fd60f1086e3b886d505cc30e92cba253c8
Deepak Srivastava <deepak.srivastava@gmail.com>
2023-12-23 21:38:34 +0530

    Initial Commit

---
+++ General/src/com/general/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.general;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
